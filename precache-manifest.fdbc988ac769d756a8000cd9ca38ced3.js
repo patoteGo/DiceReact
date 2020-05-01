7ba940605efce8641607252cf6493fd0cbba5a93
patodekaz <pato@dekaz.cl>
2020-05-01 12:32:43 -0400

    Updates

---
+++ precache-manifest.fdbc988ac769d756a8000cd9ca38ced3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc62e1f07c40ad34b654af2feb39daf2",
+    "url": "/DiceReact/index.html"
+  },
+  {
+    "revision": "fffc12d37fc919decc1f",
+    "url": "/DiceReact/static/css/main.6635fe8d.chunk.css"
+  },
+  {
+    "revision": "625fa7431628a14ffc43",
+    "url": "/DiceReact/static/js/2.3db4b0b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DiceReact/static/js/2.3db4b0b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fffc12d37fc919decc1f",
+    "url": "/DiceReact/static/js/main.885153ba.chunk.js"
+  },
+  {
+    "revision": "2ab9cb8bc06e6d5836c9",
+    "url": "/DiceReact/static/js/runtime-main.4eee01d6.js"
+  }
+]);
