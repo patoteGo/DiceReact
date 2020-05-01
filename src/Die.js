c449a7eba9daeb2729ec15a1c866cc51934264ca
patodekaz <pato@dekaz.cl>
2020-05-01 12:25:21 -0400

    changes

--- src/Die.js
+++ src/Die.js
@@ -4,7 +4,7 @@ class Die extends Component {
     render() {
         return (
            
-                <i className={`Die fas fa-dice-${this.props.face}`}> </i>
+                <i className={`Die fas fa-dice-${this.props.face} ${this.props.rolling ? 'rolling' : ''}`}> </i>
             
         
         );
--- src/RollDice.js
+++ src/RollDice.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Die from './Die'
+import Die from './Die';
+import './RollDice.css';
 
 class RollDice extends Component {
     static defaultProps = {
@@ -7,23 +8,28 @@ class RollDice extends Component {
     }
     constructor(props){
         super(props);
-        this.state = { die1: "one", die2: "two"};
+        this.state = { die1: "one", die2: "two", isRolling: "false"};
         this.roll = this.roll.bind(this);
     }
     
     roll(){
         const newDie1 = this.props.sides[Math.floor(Math.random() *  this.props.sides.length)]
         const newDie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
-        this.setState({ die1: newDie1, die2: newDie2})
+        this.setState({ die1: newDie1, die2: newDie2, rolling: true})
+        setTimeout(() => {
+            this.setState({ rolling: false})
+        }, 1000);
     }
 
     render() {
       
         return (
-            <div>
-                <Die face={this.state.die1} />
-                <Die face={this.state.die2} />
-                <button onClick={this.roll}>RollDice</button>
+            <div className="RollDice" >
+                <div className="RollDice-container">
+                    <Die face={this.state.die1} rolling={this.state.rolling}/>
+                    <Die face={this.state.die2} rolling={this.state.rolling}/>
+                </div>
+                <button disabled={this.state.rolling} onClick={this.roll}>{this.state.rolling ? 'rolling...' : 'Roll Dice'}</button>
             </div>
         )
     }
